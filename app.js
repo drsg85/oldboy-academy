cbc97b7e46803ea5e3a2791092b9bc06776b9d4f
nigel-riss <mr.kurenkov@gmail.com>
2017-10-27 16:46:10 +0200

    Hero styled up to desktop and slyder component added

---
+++ app.js
@@ -0,0 +1,212 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */,
+/* 1 */,
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _Slider = __webpack_require__(3);
+
+var _Slider2 = _interopRequireDefault(_Slider);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var headerSlider = new _Slider2.default("header-slider", true);
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Slider = function () {
+    function Slider(sliderID, createNavigation) {
+        _classCallCheck(this, Slider);
+
+        // default values
+        var _createNav = createNavigation || false;
+
+        // connecting slider
+        this.slider = document.getElementById(sliderID);
+        if (!this.slider) {
+            throw new Error("There is no id: " + sliderID + " in document");
+        }
+
+        // connecting slides
+        var maxHeight = 0;
+        console.log(this.slider.children);
+        this.slides = [];
+        for (var i = 0; i < this.slider.children.length; i++) {
+            this.slides.push(this.slider.children[i]);
+            if (this.slider.children[i].offsetHeight > maxHeight) {
+                maxHeight = this.slider.children[i].offsetHeight;
+            }
+        }
+        console.log(this.slides);
+        if (!this.slides.length) {
+            throw new Error("There is no slides in slider: " + sliderID);
+        }
+
+        // setting slider height
+        this.slider.style.height = maxHeight + "px";
+
+        // creating slider navigation
+        this.dots = [];
+        var self = this;
+        if (_createNav) {
+            // dots
+            this.navigation = document.createElement("div");
+            this.navigation.className = "slider__navigation";
+            this.slider.insertBefore(this.navigation, this.slider.firstChild);
+            for (var _i = 0; _i < this.slides.length; _i++) {
+                var dot = document.createElement("div");
+                dot.className = "slider__dot";
+                dot.index = _i;
+                dot.addEventListener("click", function () {
+                    self.currentSlide = this.index;
+                });
+                this.dots.push(dot);
+                this.navigation.appendChild(dot);
+            }
+
+            // prev button
+            this.prev = document.createElement("div");
+            this.prev.className = "slider__prev";
+            this.slider.insertBefore(this.prev, this.slider.firstChild);
+
+            // next button
+            this.next = document.createElement("div");
+            this.next.className = "slider__next";
+            this.slider.insertBefore(this.next, this.slider.firstChild);
+        }
+
+        // setting current slide
+        this.__currentSlide = 0;
+        this.currentSlide = 0;
+
+        this.events();
+    }
+
+    _createClass(Slider, [{
+        key: "events",
+        value: function events() {
+            var _this = this;
+
+            this.next.addEventListener("click", function () {
+                _this.currentSlide++;
+            });
+
+            this.prev.addEventListener("click", function () {
+                _this.currentSlide--;
+            });
+        }
+    }, {
+        key: "currentSlide",
+        set: function set(num) {
+            var maxSlideNum = this.slides.length - 1;
+            if (num > maxSlideNum) {
+                num = 0;
+            }
+            if (num < 0) {
+                num = maxSlideNum;
+            }
+            this.__currentSlide = num;
+
+            for (var i = 0; i <= maxSlideNum; i++) {
+                if (i === this.__currentSlide) {
+                    this.slides[i].classList.add("slider__slide--current");
+                    this.dots[i].classList.add("slider__dot--active");
+                } else {
+                    this.slides[i].classList.remove("slider__slide--current");
+                    this.dots[i].classList.remove("slider__dot--active");
+                }
+            }
+        },
+        get: function get() {
+            return this.__currentSlide;
+        }
+    }]);
+
+    return Slider;
+}();
+
+exports.default = Slider;
+
+/***/ })
+/******/ ]);
