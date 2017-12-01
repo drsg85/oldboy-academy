3767b943cfefef6ed40d927277fe9006d3c22ad3
nigel-riss <mr.kurenkov@gmail.com>
2017-12-01 15:55:04 +0200

    Map added and styled, contact box fix

---
+++ map.js
@@ -0,0 +1,164 @@
+;
+function initMap() {
+    var styledMapType = new google.maps.StyledMapType(
+        [
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.man_made",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#333333"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.man_made",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#e4e4e4"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.man_made",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "color": "#333333"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#5a5a5a"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "color": "#d5a353"
+                    },
+                    {
+                        "weight": "0.01"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.business",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#d5a353"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "color": "#e9e9e9"
+                    },
+                    {
+                        "weight": "0.01"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#212121"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#d5a353"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "weight": "0.01"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "weight": "0.01"
+                    },
+                    {
+                        "color": "#d5a353"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#333333"
+                    }
+                ]
+            }
+        ]
+    );
+    var coords = {lat: 55.7724663, lng: 37.676919};
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 17,
+        center: coords,
+        disableDefaultUI: true
+    });
+
+    // setting map style
+    map.mapTypes.set("styled_map", styledMapType);
+    map.setMapTypeId("styled_map");
+
+    // custom animated marker sample
+    // https://codepen.io/dylanvann/pen/yNWdxJ
+}
