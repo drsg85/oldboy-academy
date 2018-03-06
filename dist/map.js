cc6e2bd93e61668623505fc8154d2d51f062f145
Verdan777 <vereshhagin98@mail.ru>
2018-03-06 13:08:21 +0200

    Fix map styles

--- dist/map.js
+++ dist/map.js
@@ -50,6 +50,18 @@ function initMap() {
                 ]
             },
             {
+                "featureType": "administrative",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "color": "#e9e9e9"
+                    },
+                    {
+                        "weight": "0.4"
+                    }
+                ]
+            },
+            {
                 "featureType": "landscape.man_made",
                 "elementType": "geometry",
                 "stylers": [
