c869cee7a46fd611522eb60ca1a4fab90d79a57a
nigel-riss <mr.kurenkov@gmail.com>
2018-03-19 11:35:29 +0200

    Start change of js architecture Add main.js Fix SideMenu module

--- main.js
+++ main.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 5);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -251,10 +251,7 @@ var SideMenu = function () {
 exports.default = SideMenu;
 
 /***/ }),
-/* 2 */,
-/* 3 */,
-/* 4 */,
-/* 5 */
+/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -268,114 +265,10 @@ var _SideMenu = __webpack_require__(1);
 
 var _SideMenu2 = _interopRequireDefault(_SideMenu);
 
-var _FormSender = __webpack_require__(6);
-
-var _FormSender2 = _interopRequireDefault(_FormSender);
-
-var _Popup = __webpack_require__(7);
-
-var _Popup2 = _interopRequireDefault(_Popup);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var headerSlider = new _Slider2.default("header-slider", true);
-
 var sideMenu = new _SideMenu2.default();
-
-var formSender = new _FormSender2.default();
-
-var popup = new _Popup2.default();
-
-/***/ }),
-/* 6 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var FormSender = function () {
-    function FormSender() {
-        _classCallCheck(this, FormSender);
-
-        this.popup = document.querySelector(".popup");
-        this.events();
-    }
-
-    _createClass(FormSender, [{
-        key: "events",
-        value: function events() {
-            var self = this;
-            document.addEventListener("submit", function (event) {
-                event.preventDefault();
-
-                self.popup.classList.add("popup--show");
-            });
-        }
-    }]);
-
-    return FormSender;
-}();
-
-exports.default = FormSender;
-
-/***/ }),
-/* 7 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Popup = function () {
-    function Popup() {
-        _classCallCheck(this, Popup);
-
-        this.popup = document.querySelector(".popup");
-        this.closeButton = document.querySelector(".popup__close");
-        this.okButton = document.querySelector(".popup__controls a");
-
-        this.events();
-    }
-
-    _createClass(Popup, [{
-        key: "events",
-        value: function events() {
-            var _this = this;
-
-            this.closeButton.addEventListener("click", function (event) {
-                _this.close();
-            });
-
-            this.okButton.addEventListener("click", function (event) {
-                _this.close();
-            });
-        }
-    }, {
-        key: "close",
-        value: function close() {
-            this.popup.classList.remove("popup--show");
-        }
-    }]);
-
-    return Popup;
-}();
-
-exports.default = Popup;
 
 /***/ })
 /******/ ]);
