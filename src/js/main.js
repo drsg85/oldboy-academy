38eefd0c289bd069b30676e9f2b21b44c07ad871
nigel-riss <mr.kurenkov@gmail.com>
2018-04-03 16:15:42 +0200

    Restyle hero and menu for tablets portrait

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-import Slider from "./modules/Slider.js";
-import SideMenu from "./modules/SideMenu.js";
 import MobileMenu from "./modules/MobileMenu.js";
+import Slider from "./modules/Slider.js";
+// import SideMenu from "./modules/SideMenu.js";
 import FormSender from "./modules/FormSender.js";
 
 new MobileMenu();
 new Slider("header-slider", true);
-new SideMenu();
+// new SideMenu();
 new FormSender();
