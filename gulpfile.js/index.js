56e5610ebf15e67c656cf05bafbe4541cba43eb6
nigel-riss <mr.kurenkov@gmail.com>
2019-04-02 12:15:08 +0200

    Fix hero max height

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -34,20 +34,6 @@ const webpackConfig = {
   output: {
       filename: "[name].js"
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.m?js$/,
-  //       exclude: /(node_modules|bower_components)/,
-  //       use: {
-  //         loader: 'babel-loader',
-  //         options: {
-  //           presets: ['@babel/preset-env']
-  //         }
-  //       }
-  //     }
-  //   ]
-  // }
 };
 
 /**
